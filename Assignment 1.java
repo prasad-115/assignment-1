528f9075084f0f824c0d46f3d08a7d3c5d5379ee
prasad-115 <164524659+prasad-115@users.noreply.github.com>
2024-03-28 17:55:11 +0530

    Add files via upload

---
+++ Assignment 1.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Assignment1{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Hello Worlld!");
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        if(n1==n2){
+            System.out.println("Values are equal");
+        }
+        else{
+            System.out.println("Values are not equal");
+        }
+        int n3=sc.nextInt();
+        int n4=sc.nextInt();
+        int n5=sc.nextInt();
+        System.out.println(n3+n4+n5);
+        System.out.println((n3+n4+n5)/3);
+        System.out.println(n3-n4-n5);
+        System.out.println(n3*n4*n5);
+    }
+}
+    
+
+
+
